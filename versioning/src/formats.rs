5fa221e7e58ff0c9ac90f75a72c6155ff8d87f27
Timothy Bess <tim@prospective.dev>
2023-05-10 15:33:31 -0400

    snakecase

--- versioning/src/formats.rs
+++ versioning/src/formats.rs
@@ -4,14 +4,14 @@ use serde::{Serialize, Deserialize};
 
 use crate::{SerializeFormat, DeserializeFormat};
 
-#[cfg(feature = "serde-json")]
+#[cfg(feature = "serde_json")]
 impl SerializeFormat for serde_json::Value {
     fn versioned_serialize<T: Serialize>(data: T) -> Result<Self, Box<dyn std::error::Error>> {
         Ok(serde_json::to_value(&data)?)
     }
 }
 
-#[cfg(feature = "serde-json")]
+#[cfg(feature = "serde_json")]
 impl DeserializeFormat for serde_json::Value {
     fn versioned_deserialize<'a, T>(&'a self) -> Result<T, Box<dyn std::error::Error>>
     where
@@ -23,7 +23,7 @@ impl DeserializeFormat for serde_json::Value {
 
 
 /// Zero copy wrapper for MessagePack bytes stored as a [std::borrow::Cow] of bytes.
-#[cfg(feature = "serde-rmp")]
+#[cfg(feature = "serde_rmp")]
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct MsgPackBytes<'a>(
     #[serde(with = "serde_bytes")]
@@ -31,14 +31,14 @@ pub struct MsgPackBytes<'a>(
     pub Cow<'a, [u8]>,
 );
 
-#[cfg(feature = "serde-rmp")]
+#[cfg(feature = "serde_rmp")]
 impl SerializeFormat for MsgPackBytes<'_> {
     fn versioned_serialize<T: Serialize>(data: T) -> Result<Self, Box<dyn std::error::Error>> {
         Ok(MsgPackBytes(Cow::Owned(rmp_serde::to_vec(&data)?)))
     }
 }
 
-#[cfg(feature = "serde-rmp")]
+#[cfg(feature = "serde_rmp")]
 impl<'a> DeserializeFormat for MsgPackBytes<'a> {
     fn versioned_deserialize<'b, T: Deserialize<'b>>(
         &'b self,
